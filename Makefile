9133103f8a7bafb49bf2dfaf7c872eb72219a35a
Calvin Atewamba <calvin.atewamba@gainx.com>
2023-11-05 02:02:08 -0500

    adjust Makefile

--- Makefile
+++ Makefile
@@ -11,8 +11,8 @@ format:
 lint:
 	pylint --disable=R,C --ignore-patterns=test_.*?py *.py mylib/*.py
 
-container-lint:
-	docker run --rm -i hadolint/hadolint < Dockerfile
+#container-lint:
+#	docker run --rm -i hadolint/hadolint < Dockerfile
 
 refactor: format lint
 
--- test_main.py
+++ test_main.py
@@ -19,5 +19,9 @@ def test_help():
     assert result.exit_code == 0
     assert "Show this message and exit." in result.output
 
-
+def test_add_cli():
+    runner = CliRunner()
+    result = runner.invoke(add_cli, ["1", "2"])
+    assert result.exit_code == 0
+    assert "3" in result.output
 # write a test for the add_cli function that calls the add_cli function with the arguments 1 and 2 and checks that the output is 3.
